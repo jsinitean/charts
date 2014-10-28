0af69f6d52a20a6e7ac23507b9a61cddbd4b9e90
Jeremy Sinitean <jeremy.sinitean@gmail.com>
2014-10-27 22:15:42 -0500

    Limiting view results

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -7,65 +7,11 @@ class PublicController < ApplicationController
     Asana.configure do |client|
       client.api_key = 'jlVkWF3.0RBcLBYXbvN5wZeHn0GCEgJN'
     end
-      
-    #@users = Asana::User.me
-    #@tags = Asana::Tag.find('9393480861390')
-    #@tasks = @tags.tasks
-    @pro1 = Asana::Project.find('9491387612778')
-    @pro2 = Asana::Project.find('9491387612780')
-    @pro3 = Asana::Project.find('9491387612782')
+
+    @pro1 = Asana::Project.find('18650197634428')
+    @pro2 = Asana::Project.find('18641181153318')
     @tasks1 = @pro1.tasks
     @tasks2 = @pro2.tasks
-    @tasks3 = @pro3.tasks
-    
-    #require "rubygems"
-    #require "JSON"
-    #require "net/https"
-    #require "uri"
-    #api_key = "jlVkWF3.0RBcLBYXbvN5wZeHn0GCEgJN"
-    #workspace_id = "137577850971"
-
-    # set up HTTPS connection
-    #uri = URI.parse("https://app.asana.com/api/1.0/tags/9191758976196/tasks?opt_fields=name,completed,assignee")
-    #http = Net::HTTP.new(uri.host, uri.port)
-    #http.use_ssl = true
-    #http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-
-    # set up the request
-    #header = {
-    #  "Content-Type" => "application/json"
-    #}
-    
-    #fullPath = uri.path+'?'+uri.query
-    
-    #req = Net::HTTP::Get.new(fullPath)
-    #req.basic_auth(api_key, '')
-    #res = http.start { |http| http.request(req) }
-    #@object = res.body
-    # output
-    #print http.request(req)
-
-    #objectResult = JSON.parse(res.body)
-    #objectPretty = JSON.pretty_generate(objectResult)
-    #@object = objectPretty  
-    
-    @sprint1 = Sprints.ui.range
-    @sprint_count1 = @sprint1.average(:completed_pts)
-    @sprint_count_round1 = @sprint_count1.round(1)
-    @sprint_hours1 = @sprint1.average(:hours)
-    @sprint_hours_round1 = @sprint_hours1.round(1)
-
-    @sprint2 = Sprints.dev.range.team2
-    @sprint_count2 = @sprint2.average(:completed_pts)
-    @sprint_count_round2 = @sprint_count2.round(1)
-    @sprint_hours2 = @sprint2.average(:hours)
-    @sprint_hours_round2 = @sprint_hours2.round(1)
-    
-    @sprint3 = Sprints.dev.range.team1
-    @sprint_count3 = @sprint3.average(:completed_pts)
-    @sprint_count_round3 = @sprint_count3.round(1)
-    @sprint_hours3 = @sprint3.average(:hours)
-    @sprint_hours_round3 = @sprint_hours3.round(1)
     
   end
   
@@ -78,14 +24,7 @@ class PublicController < ApplicationController
   
     @pro2 = Asana::Project.find('11125477959366')
     @tasks2 = @pro2.tasks
-    
-    @sprint = Sprints.dev
-    @sprint_count = @sprint.average(:completed_pts)
-    @sprint_count_round = @sprint_count.round(1)
-    @sprint_hours = @sprint.average(:hours)
-    @sprint_hours_round = @sprint_hours.round(1)
-    @sprint_bugs = @sprint.average(:bugs)
-    @sprint_bugs_round = @sprint_bugs.round(1)
+
   end
   
   def graph3
@@ -97,16 +36,7 @@ class PublicController < ApplicationController
       
     @pro1 = Asana::Project.find('11125477959364')
     @tasks1 = @pro1.tasks
-    
-    @pro3 = Asana::Project.find('11314511713050')
-    @tasks2 = @pro3.tasks
-    #@sprint = Sprints.ui
-    #@sprint_count = @sprint.average(:completed_pts)
-    #@sprint_count_round = @sprint_count.round(1)
-    #@sprint_hours = @sprint.average(:hours)
-    #@sprint_hours_round = @sprint_hours.round(1)
-    #@sprint_bugs = @sprint.average(:bugs)
-    #@sprint_bugs_round = @sprint_bugs.round(1)
+
   end
   
   def create
